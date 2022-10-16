120ed5b6f40803030add7731129c3001642c15d1
irjala <jonasirjala@gmail.com>
2022-10-16 18:39:24 +0300

    SETUP funkar

--- models/service.js
+++ models/service.js
@@ -1,10 +1,12 @@
 const Service = require('../setup/SETUP_S')
 
-const { Sequalize } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 //const sequelize = new Sequelize('postgresql://fwtbpmva:d498ee036dbd5c5537e4@128.214.253.167:5432/fwtbpmva')
 const sequelize = new Sequelize('postgresql://fwtbpmva:d498ee036dbd5c5537e4@128.214.253.167:5432/fwtbpmva')
 
-const getServices = await Service.findAll();
+module.exports = (sequelize) => {
+    const getServices = async() => await Service.findAll();
 
-module.exports = mongoose.model('Service', getServices)
+    return getServices;
+}
--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 const express = require('express')
     //const bodyParser = require('body-parser');
-const sequelize = require('sequelize')
+const { Sequelize } = require('sequelize')
 const app = express();
 const pg = require('pg');
 require('dotenv').config()
 
+const sequelize = new Sequelize('postgresql://fwtbpmva:d498ee036dbd5c5537e4@128.214.253.167:5432/fwtbpmva')
 const PORT = process.env.PORT || 3031
 
-var conString = "postgresql://qdwephxk:ed9a29511c3fa2de1d2a@128.214.253.167:5432/qdwephxk";
+const setupO = require(`${__dirname}/setup/setup_O`)(sequelize)
+const SetupS = require(`${__dirname}/setup/setup_S`)(sequelize)
+const setServices = new SetupS;
 
-var client = new pg.Client(conString);
-client.connect();
+(async() => {
 
-client.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    client.end()
-})
+    await sequelize.sync({ force: true });
+
+    // Code here
+})();
+
+
+//var conString = "postgresql://qdwephxk:ed9a29511c3fa2de1d2a@128.214.253.167:5432/qdwephxk";
+//
+//var client = new pg.Client(conString);
+//client.connect();
+//
+//client.query('SELECT NOW()', (err, res) => {
+//    console.log(err, res)
+//    client.end()
+//})
+
+
+
+
 
 app.use(express.json())
 
-const serviceRouter = require('./routes/services')
+const serviceRouter = require('./routes/services');
+
 app.use('/services', serviceRouter)
 
 app.listen(PORT, () => {
