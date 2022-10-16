ebf8e82c7b7bf12dbbad6582a07e488c866842cb
irjala <jonasirjala@gmail.com>
2022-10-16 18:49:28 +0300

    clean + comment

--- server.js
+++ server.js
@@ -8,16 +8,11 @@ require('dotenv').config()
 const sequelize = new Sequelize('postgresql://fwtbpmva:d498ee036dbd5c5537e4@128.214.253.167:5432/fwtbpmva')
 const PORT = process.env.PORT || 3031
 
+// Dessa calls kör setup av databaserna IFALL de inte redan existerar.
+// Async funktion await sequelize.sync({ force: true }); körs i SetupS så att den direkt försöker skapa ifall det inte existerar
 const setupO = require(`${__dirname}/setup/setup_O`)(sequelize)
 const SetupS = require(`${__dirname}/setup/setup_S`)(sequelize)
-const setServices = new SetupS;
 
-(async() => {
-
-    await sequelize.sync({ force: true });
-
-    // Code here
-})();
 
 
 //var conString = "postgresql://qdwephxk:ed9a29511c3fa2de1d2a@128.214.253.167:5432/qdwephxk";
